0b4b6d9955bee71f6f81a4aa430b483f7e4e48ba
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-07-07 14:06:53 +0300

    Adding fragments, drawable pictures, class

--- app/src/main/java/com/germiyanoglu/android/findl/activity/SplashScreenActivity.java
+++ app/src/main/java/com/germiyanoglu/android/findl/activity/SplashScreenActivity.java
@@ -172,7 +172,7 @@ public class SplashScreenActivity extends AppCompatActivity implements GoogleApi
                             .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
-                                        finish();
+                                    finish();
                                 }
                             }).show();
                 } else
@@ -186,6 +186,50 @@ public class SplashScreenActivity extends AppCompatActivity implements GoogleApi
             // TODO : 211 ) Calling Location
             enableLocation();
 
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            mCurrentLocation = String.valueOf(location.getLatitude()
+                                    + "," + String.valueOf(location.getLongitude()));
+                        } else {
+                            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                                    ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                // TODO: Consider calling
+                                //    ActivityCompat#requestPermissions
+                                // here to request the missing permissions, and then overriding
+                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                //                                          int[] grantResults)
+                                // to handle the case where the user grants the permission. See the documentation
+                                // for ActivityCompat#requestPermissions for more details.
+                                return;
+                            }
+                            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, Looper.myLooper());
+
+                        }
+
+                    }
+                });
+
+
+        Log.d(TAG, "Current Location in SplashScreen Activity : " + mCurrentLocation);
 
         mLocationSharedPreferences = getApplicationContext().getSharedPreferences(
                 GoogleMapApi.CURRENT_LOCATION_SHARED_PREFERENCE_KEY, 0);

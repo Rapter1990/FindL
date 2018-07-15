62df657052109c9806fb8cc959da1683458aa215
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-07-16 01:13:20 +0300

    Fix problem

--- app/src/main/java/com/germiyanoglu/android/findl/activity/LocationDetailActivity.java
+++ app/src/main/java/com/germiyanoglu/android/findl/activity/LocationDetailActivity.java
@@ -3,6 +3,7 @@ package com.germiyanoglu.android.findl.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
@@ -18,6 +19,7 @@ import android.widget.ProgressBar;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.germiyanoglu.android.findl.R;
 import com.germiyanoglu.android.findl.adapter.ViewPagerAdapter;
@@ -27,8 +29,16 @@ import com.germiyanoglu.android.findl.utils.AppController;
 import com.germiyanoglu.android.findl.utils.GoogleMapApi;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -75,7 +85,7 @@ public class LocationDetailActivity extends AppCompatActivity {
         // TODO 270 ) Define getAllLocationInformationFromJSON
         getAllLocationInformationFromJSON(mCurrentLocationDetailUrl);
 
-
+        //new JSONParse().execute();
     }
 
 
@@ -247,4 +257,173 @@ public class LocationDetailActivity extends AppCompatActivity {
         AppController.getInstance().addToRequestQueue(request, getResources().getString(R.string.jsonArrayTag));
 
     }
+
+
+    private class JSONParse extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            URL url;
+            try {
+                url = new URL(mCurrentLocationDetailUrl);
+                urlConnection.setRequestMethod("GET"); //Your method here
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line + "\n");
+
+                if (buffer.length() == 0)
+                    return null;
+
+                return buffer.toString();
+            } catch (IOException e) {
+                Log.e(TAG, "IO Exception", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String response) {
+            if(response != null) {
+                try{
+                    JSONObject json = new JSONObject(response);
+
+                    JSONObject rootJsonObject = json.getJSONObject("result");
+
+                    String currentLocationId = rootJsonObject.getString("place_id");
+
+                    String currentLocationName = rootJsonObject.getString("name");
+
+                    Double currentLocationLatitude = rootJsonObject
+                            .getJSONObject("geometry").getJSONObject("location")
+                            .getDouble("lat");
+
+                    Double currentLocationLongitude = rootJsonObject
+                            .getJSONObject("geometry").getJSONObject("location")
+                            .getDouble("lng");
+
+                    String currentLocationOpeningHourStatus = rootJsonObject
+                            .has("opening_hours") ? rootJsonObject.getJSONObject("opening_hours")
+                            .getString("open_now") : "Status Not Available";
+                    if (currentLocationOpeningHourStatus.equals("true"))
+                        currentLocationOpeningHourStatus = getString(R.string.open_now);
+                    else if (currentLocationOpeningHourStatus.equals("false"))
+                        currentLocationOpeningHourStatus = getString(R.string.closed);
+
+                    Double currentLocationRating = rootJsonObject.has("rating") ?
+                            rootJsonObject.getDouble("rating") : 0;
+
+                    String currentLocationAddress = rootJsonObject.has("formatted_address") ?
+                            rootJsonObject.getString("formatted_address") :
+                            "Address Not Available";
+
+                    String currentLocationPhoneNumber = rootJsonObject
+                            .has("international_phone_number") ? rootJsonObject
+                            .getString("international_phone_number") :
+                            "Not defined Phone Number";
+
+                    String currentLocationWebsite = rootJsonObject.has("website") ?
+                            rootJsonObject.getString("website") :
+                            "Not defined Website";
+
+                    String currentLocationShareLink = rootJsonObject.getString("url");
+                    mLocationShareUrl = currentLocationShareLink;
+
+                    Location currentlocation = new Location();
+                    currentlocation.setmLocationId(currentLocationId);
+                    currentlocation.setmLocationName(currentLocationName);
+                    currentlocation.setmLocationLatitude(currentLocationLatitude);
+                    currentlocation.setmLocationLongitude(currentLocationLongitude);
+                    currentlocation.setmLocationOpeningHourStatus(currentLocationOpeningHourStatus);
+                    currentlocation.setmLocationRating(currentLocationRating);
+                    currentlocation.setmLocationAddress(currentLocationAddress);
+                    currentlocation.setmLocationPhoneNumber(currentLocationPhoneNumber);
+                    currentlocation.setmLocationWebsite(currentLocationWebsite);
+                    currentlocation.setmLocationShareLink(currentLocationShareLink);
+
+                    if (rootJsonObject.has("reviews")) {
+
+                        JSONArray reviewJsonArray = rootJsonObject.getJSONArray("reviews");
+                        for (int i = 0; i < reviewJsonArray.length(); i++) {
+                            JSONObject currentUserRating = (JSONObject) reviewJsonArray.
+                                    get(i);
+
+                            String userName = currentUserRating.getString("author_name");
+                            String userProfilePhoto = currentUserRating.getString("profile_photo_url");
+                            Double userRating = currentUserRating.getDouble("rating");
+                            String userDescriptionTime = currentUserRating.getString("relative_time_description");
+                            String userDescription = currentUserRating.getString("text");
+
+                            UserLocationRating currentLocationUserRating = new UserLocationRating();
+                            currentLocationUserRating.setmUserName(userName);
+                            currentLocationUserRating.setmUserProfilePictureUrl(userProfilePhoto);
+                            currentLocationUserRating.setmUserPlaceRating(userRating);
+                            currentLocationUserRating.setmUserRatingDateTime(userDescriptionTime);
+                            currentLocationUserRating.setmAuthorReviewText(userDescription);
+
+                            mPlaceUserRatingsArrayList.add(currentLocationUserRating);
+                        }
+                    }
+
+                    // TODO 273 )  Defining bundle from location information and review
+                    Bundle currentLocationDetailData = new Bundle();
+                    currentLocationDetailData.putParcelable(
+                            GoogleMapApi.CURRENT_LOCATION_DATA, currentlocation);
+
+                    Bundle currentLocationUserRatingDetail = new Bundle();
+                    currentLocationUserRatingDetail.putParcelableArrayList(
+                            GoogleMapApi.CURRENT_LOCATION_USER_RATING,
+                            mPlaceUserRatingsArrayList);
+
+
+                    // TODO 274 )  Setting these bundle in the viewpager
+                    ViewPagerAdapter viewpagerAdapter = new ViewPagerAdapter(
+                            getSupportFragmentManager(),mContext);
+                    viewpagerAdapter.setBundleData(currentLocationDetailData, currentLocationUserRatingDetail);
+                    viewPager.setAdapter(viewpagerAdapter);
+
+                    // TODO 275 )  Setting viewpager to tablayout
+                    tabLayout.setupWithViewPager(viewPager);
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+
+
 }
